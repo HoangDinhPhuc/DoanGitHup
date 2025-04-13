5a74715084a633e6c486056206eb23e598cfb0b7
trandinhhoangg <trandinhhoang150103@gmail.com>
2025-04-13 20:21:36 +0700

    them ham getPageStory

--- lib/service/book_repository.dart
+++ lib/service/book_repository.dart
@@ -2,4 +2,32 @@ import 'http_service.dart';
 import 'models/book_detail_model.dart';
 import 'models/book_model.dart';
 
-class BookRepository {}
+class BookRepository {
+    String domainMain = 'https://img.otruyenapi.com/';
+  String domainFE = 'https://otruyen.cc';
+  String domainCDN = 'https://img.otruyenapi.com';
+  String urlThumbnail = 'https://img.otruyenapi.com/uploads/comics/';
+
+  FetchClient service = FetchClient();
+
+  Future<List<BookModel>> getPageStory(int page) async {
+    final response = await service.getData(
+        // danh sách truyện
+        domainApp: 'https://otruyenapi.com/v1/api/',
+        path: 'danh-sach/ngon-tinh?page=$page');
+    List<BookModel> books = [];
+    if (response.statusCode == 200) {
+      final data = response.data['data']['items'];
+      for (var e in data) {
+        books.add(BookModel(
+            slug: e['slug'],
+            idBook: e['_id'],
+            nameBook: e['name'],
+            thumbNailURL: e['thumb_url']));
+      }
+      return books;
+    } else {
+      return [];
+    }
+  }
+}
