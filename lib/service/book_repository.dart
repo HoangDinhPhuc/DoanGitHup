70ece197155dcaa992b05d7c5286c24b3704eaed
Duy-Loc <nguyenduyloc112233@gmail.com>
2025-04-13 20:27:12 +0700

    them ham getDetailStory

--- lib/service/book_repository.dart
+++ lib/service/book_repository.dart
@@ -30,4 +30,45 @@ class BookRepository {
       return [];
     }
   }
+  Future<BookDetailModel> getDetailStory(String slug) async {
+    final response = await service.getData(
+        // danh sách truyện
+        domainApp: 'https://otruyenapi.com/v1/api/',
+        path: 'truyen-tranh/$slug');
+
+    if (response.statusCode == 200) {
+      final data = response.data['data']['item'];
+      List<String> categories = [];
+      List<String> chaptersURL = [];
+      for (var e in data['category']) {
+        categories.add(e['name']);
+      }
+      if (data['chapters'].isEmpty) {
+        return BookDetailModel(
+            name: data['name'],
+            author: data['author'][0],
+            catogories: categories,
+            slug: data['slug'],
+            quanityChapters: chaptersURL.length,
+            descriptionHead: data['content'],
+            chapterUrls: []);
+      } else {
+        for (var e in data['chapters'][0]['server_data']) {
+          chaptersURL.add(e['chapter_api_data']);
+        }
+      }
+
+      return BookDetailModel(
+          name: data['name'],
+          author: data['author'][0],
+          catogories: categories,
+          slug: data['slug'],
+          quanityChapters: chaptersURL.length,
+          descriptionHead: data['content'],
+          chapterUrls: chaptersURL);
+    } else {
+      return BookDetailModel();
+    }
+  }
+
 }
